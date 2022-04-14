abc8e86dc4804e9e72802d1b5e7a61fd51ede318
tranhoangthacba8 <tranhoangthacba8@gmail.com>
2022-04-14 16:00:13 +0700

    feat(auth): migrate: create table users

---
+++ database/migrations/2022_04_14_072559_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('workingType');
+            $table->dateTime('workingTime');
+            $table->integer('project');
+            $table->string('detail');
+            $table->dateTime('date');
+            $table->integer('userId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('testLogin',function (){
+   return view('authen.loginForm');
+});
